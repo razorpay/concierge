67c136381493dda793c282328baf00677b086167
Harshil Mathur <harshilmathur@gmail.com>
2014-04-21 15:46:13 +0530

    Added Topbar

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -30,14 +30,29 @@
         @endif
 
         @if(Auth::check())
-            <div class="container">
-                <p class="center">
-                    <a href="{{ URL::to('/')}}"> Home</a>
-                    <a href="{{ URL::to('/password')}}"> Change Password</a>
-                    <a href="{{ URL::to('/logout') }}">Logout</a>
+            <nav class="navbar navbar-inverse" role="navigation">
+              <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="{{ URL::to('/')}}">Home</a>
+                </div>
 
-                </p>
-            </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><p class="navbar-text">Signed in as {{{Auth::user()->username}}}</p></li>
+                    <li><a href="{{ URL::to('/password')}}"> Change Password</a></li>
+                    <li><a href="{{ URL::to('/logout') }}">Logout</a></li>
+                  </ul>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
             <div class="container">
                 @yield('content')
             </div>
