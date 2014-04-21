3ff80f378a63442258ce217c248c20afc1b1d00c
Harshil Mathur <harshilmathur@gmail.com>
2014-04-21 12:49:18 +0530

    Register provider for password strength validator

--- app/config/app.php
+++ app/config/app.php
@@ -120,6 +120,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Aws\Laravel\AwsServiceProvider',
+		'Schuppo\PasswordStrength\PasswordStrengthServiceProvider',
 
 	),
 
