bed9fa7193deb21d172540e05b32bce98169ce90
Harshil Mathur <harshilmathur@gmail.com>
2014-04-23 19:23:20 +0530

    Better structured routes file

--- app/routes.php
+++ app/routes.php
@@ -11,34 +11,41 @@
 |
 */
 
+//Routes for logged in user
 Route::group(array('before' => 'auth'), function()
 {
-Route::get('/groups', 'HomeController@getGroups');
+	Route::get('/groups', 'HomeController@getGroups');
 
-Route::get('/manage/{group_id}', 'HomeController@getManage');
+	Route::get('/manage/{group_id}', 'HomeController@getManage');
 
-Route::post('/manage/{group_id}', array('before'=>'csrf', 'uses'=>'HomeController@postManage'));
+	Route::post('/manage/{group_id}', array('before'=>'csrf', 'uses'=>'HomeController@postManage'));
 
-Route::post('/manage/{group_id}/terminate', array('before'=>'csrf', 'uses'=>'HomeController@postTerminate'));
+	Route::post('/manage/{group_id}/terminate', array('before'=>'csrf', 'uses'=>'HomeController@postTerminate'));
 
-Route::get('/logout', 'HomeController@getLogout');
+	Route::get('/logout', 'HomeController@getLogout');
 
-Route::get('/password', 'HomeController@getPassword');
+	Route::get('/password', 'HomeController@getPassword');
 
-Route::post('/password', array('before'=>'csrf', 'uses'=>'HomeController@postPassword'));
-
-Route::get('/users', array('before'=>'admin', 'uses'=>'HomeController@getUsers'));
-
-Route::post('/users', array('before'=>'admin|csrf', 'uses'=>'HomeController@postUsers'));
+	Route::post('/password', array('before'=>'csrf', 'uses'=>'HomeController@postPassword'));
 });
 
+//Routes for site admin
+Route::group(array('before' => 'auth|admin'), function()
+{
+	Route::get('/users', 'HomeController@getUsers');
+
+	Route::post('/users', array('before'=>'csrf', 'uses'=>'HomeController@postUsers'));
+
+});
+
+//Routes for non-logged in user
 Route::group(array('before' => 'guest'), function()
 {
-Route::get('/', 'HomeController@getIndex');
+	Route::get('/', 'HomeController@getIndex');
 
-Route::post('/signin', array('before'=>'csrf', 'uses'=>'HomeController@postSignin'));
+	Route::post('/signin', array('before'=>'csrf', 'uses'=>'HomeController@postSignin'));
 
-Route::post('/duologin', 'HomeController@postDuologin');
+	Route::post('/duologin', 'HomeController@postDuologin');
 
 });
 
