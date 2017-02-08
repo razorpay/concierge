8571f1e9fbeabc6a632d2affd8308eccca2ee95c
Abhay Rana <me@captnemo.in>
2017-02-08 14:25:43 +0530

    Finishes most configuration changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -644,7 +644,7 @@ class HomeController extends BaseController
 
             Mail::queue('emails.notification', $data, function($message)
             {
-                $message->to(config('custom_config.notification_emailid'), 'Security Notification' )->subject('Secure Access Lease Created');
+                $message->to(config('concierge.notification_emailid'), 'Security Notification' )->subject('Secure Access Lease Created');
             });
         }
         else
@@ -660,7 +660,7 @@ class HomeController extends BaseController
 
             Mail::queue('emails.notification', $data, function($message)
             {
-                $message->to(config('custom_config.notification_emailid'), 'Security Notification' )->subject('Secure Access Lease Terminated');
+                $message->to(config('concierge.notification_emailid'), 'Security Notification' )->subject('Secure Access Lease Terminated');
             });
         }
     }
--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -4,18 +4,20 @@ class DatabaseSeeder extends Seeder {
 
 	/**
 	 * Run the database seeds for setting up inital set of users
-     * 
+     *
 	 *
 	 * @return void
 	 */
 	public function run()
 	{
 		Eloquent::unguard();
-        $users=Config::get('custom_config.users');
+
+        $users=Config::get('concierge.users');
+
         foreach($users as &$user)
         {
-            $user['created_at']=date('Y-m-d H:i:s');
-            $user['updated_at']=date('Y-m-d H:i:s');
+            $user['created_at'] = date('Y-m-d H:i:s');
+            $user['updated_at'] = date('Y-m-d H:i:s');
         }
         DB::table('users')->insert($users);
 
