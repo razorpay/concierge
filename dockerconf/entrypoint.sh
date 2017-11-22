4d4a94631458a7bafb5fd6e4be68f045810e1415
Nemo <me@captnemo.in>
2017-11-22 14:19:21 +0530

    Build improvements

--- dockerconf/entrypoint.sh
+++ dockerconf/entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# wait for db to be provisioned
-sleep 30
-
-echo "Creating Log Files"
-mkdir -p /var/log/nginx
-
-# fix permissions
-chown -R nginx.nginx /app
-
 # TODO: merge concierge.nginx.conf & concierge.nginx.dev.conf in a single j2 template
 # once `alohomora` has simple jinja2 render capabilities
 # without credstash lookups
@@ -38,13 +29,6 @@ else
   cp dockerconf/concierge.nginx.conf /etc/nginx/conf.d/concierge.conf
   cp dockerconf/fastcgi.conf /etc/nginx/
 fi
-
-# Fix permissions
-echo  "$(date) Fix Storage permissions"
-cd /app/ && chmod -R o+wx storage/
-
-mkdir -p /run/
-chown 0755 /run/
 
 /usr/sbin/php-fpm7
 /usr/sbin/nginx -g 'daemon off;'
