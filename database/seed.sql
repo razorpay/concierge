fcf6767e33665d73f2b172079814a7e02b4e87fc
Aravee <aravindhan.a@razorpay.com>
2019-09-26 16:40:42 +0530

    changed e-mail

--- database/seed.sql
+++ database/seed.sql
@@ -109,7 +109,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Admin Name','admin','',1,NULL,'2016-05-18 00:30:59','2016-05-18 00:30:59',NULL,'','aravindhan.a@razorpay.com','');
+INSERT INTO `users` VALUES (1,'Admin Name','admin','',1,NULL,'2016-05-18 00:30:59','2016-05-18 00:30:59',NULL,'','abhay.rana@razorpay.com','');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
