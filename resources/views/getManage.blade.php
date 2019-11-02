528e3759c0868ba6e7d7c8c0351670ade0d10a3d
Prashanth YV <prashanth.yv@razorpay.com>
2019-11-02 15:01:03 +0530

    Make 6hrs default selected option

--- resources/views/getManage.blade.php
+++ resources/views/getManage.blade.php
@@ -182,9 +182,9 @@
 			    <label for="expiry" class="col-sm-4 control-label">Expiry:</label>
 			    <div class="col-sm-4">
 				    <select name="expiry" class="form-control" required>
-					  <option value="3600" selected>1 hour</option>
+					  <option value="3600">1 hour</option>
 					  <option value="14400">4 hours</option>
-					  <option value="21600">6 hours</option>
+					  <option value="21600" selected>6 hours</option>
 					</select>
 				</div>
 				<div class="col-sm-4">
